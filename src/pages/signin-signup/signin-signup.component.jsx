54325d82895939207cb727a71d03ab39c9d771ad
nirmaldalmia <nirmaldalmia17@gmail.com>
2019-08-01 01:38:52 +0530

    integrated signup component & firebase

--- src/pages/signin-signup/signin-signup.component.jsx
+++ src/pages/signin-signup/signin-signup.component.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 import './signin-signup.styles.scss'
 import SignIn from '../../components/signin/signin.component'
+import Signup from '../../components/signup/signup.component';
 
 const SigninSignupPage = () => (
     <div className='signin-signup'>
         <SignIn />
+        <Signup />
     </div>
 );
 
